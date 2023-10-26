d0e4d70b0f1bc6be92bd1993c3df9249efb85a6b
Denny Afrizal <denny.afrizal713@gmail.com>
2023-10-26 09:36:49 +0700

    Minor Modify

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+import pymysql
 
 def main():
     """Run administrative tasks."""
@@ -15,6 +15,9 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    
+    pymysql.install_as_MySQLdb()
+
     execute_from_command_line(sys.argv)
 
 
